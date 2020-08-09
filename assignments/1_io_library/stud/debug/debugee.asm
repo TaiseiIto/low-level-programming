f7c4d723efa42a4dc86458b7ddfc89058e76f0cf
TaiseiIto <taisei1212@outlook.jp>
2020-08-09 17:23:26 +0900

    2.7 debug read_word

--- assignments/1_io_library/stud/debug/debugee.asm
+++ assignments/1_io_library/stud/debug/debugee.asm
@@ -1,66 +1,74 @@
 global _start
 
-char_0 equ 0x30
+char_carriage_return equ 0x0d
+char_minus equ 0x2d
+char_new_line equ 0x0a
+char_space equ 0x20
+char_tab equ 0x09
+char_zero equ 0x30
+stdin equ 0
 stdout equ 1
+syscall_read equ 0
 syscall_write equ 1
 syscall_exit equ 60
 
 section .text
 
-print_uint:			;void print_uint(unsigned long rdi:integer)//print integer by decimal system
+read_word:			;char *read_word(char *rdi:buf, unsigned long rsi:sz):(if success, buf. else NULL),rdx:(length of word)//read word consisting of char except ' ', '\t', '\a' and CR
 				;{
     push rbp			;	*(rsp -= 8) = rbp;
     mov rbp, rsp		;	rbp = rsp;
-    mov rax, rdi		;	rax = rdi:integer;
-    mov r8, 10			;	r8:divisor = 10;
-.division_16_digits:		;.division_16_digits:
-    mov rcx, 0			;	rcx:(num of stored r9:(16 decimal digits) bits) = 0;
-.division_1_digit:		;.division_1_digit:
-    xor rdx, rdx		;	(rdx ^= rdx):(rdx = 0);
-    div r8			;	rdx = rax:rdi % r8:10; rax = rax:rdi / r8:10;
-    shl r9, 4			;	r9 <<= 4;
-    add r9b, dl			;	r9 += rdx:remainder;
-    add rcx, 4			;	rcx += 4;
-    test rax, rax		;	if(rax == 0)goto .print;
-    jz .print			;
-    cmp rcx, 64			;	if(rcx != 64)goto .division_1_digit;
-    jne .division_1_digit	;
-    push r9			;	*(rsp -= 8) = r9:(16 decimal digits);
-    jmp .division_16_digits	;	goto .division_16_digits;
-.print:				;.print
-    test rcx, rcx		;	if(rcx == 0)goto .print_next_16_digits;
-    jz .print_next_16_digits	;
-.print_1_digit:			;.print_1_digit:
-    mov r10, r9			;	r10:(decimal digit) = r9;
-    and r10, 0x000000000000000f	;	r10 &= 0x000000000000000f;
-    add r10, char_0		;	r10 += '0';
-    push r10			;	*(rsp -= 8) = r10;
-    mov rax, syscall_write	;	rax = syscall_write;
-    mov rdi, stdout		;	rdi = stdout;
-    mov rsi, rsp		;	rsi = rsp;
+    mov r8, rdi			;	r8 = rdi:buf;
+    mov r9, rsi			;	r9 = rsi:sz;
+    test r9, r9			;	if(!r9)goto .error;
+    jz .error			;
+    dec r9			;	r9--;//r9 = max strlen
+    xor rcx, rcx		;	rcx:(num of read bytes) = 0;
+    mov rax, syscall_read	;	rax = syscall_read;
+    mov rdi, stdin		;	rdi = stdin;
+    mov rsi, r8			;	rsi = r8:buf;
     mov rdx, 1			;	rdx = 1;
+.read:				;.read:
+    cmp rcx, r9			;	if(rcx:(num of read bytes) == r9:(max strlen))goto .error;
+    je .error			;
     push rcx			;	*(rsp -= 8) = rcx;//protect rcx from syscall
-    syscall			;	rax = write(rdi:stdout, rsi:&(decimal digit), rdx:1);
-    pop rcx			;	rsp += 8; rcx = *rsp;//recover rcx
-    pop r10			;	rsp += 8; r10 = *rsp;
-    shr r9, 4			;	r9 >>= 4;
-    sub rcx, 4			;	rcx -= 4;
-    jmp .print			;	goto .print;
-.print_next_16_digits:		;.print_next_16_digits:
-    cmp rbp, rsp		;	if(rbp == rsp)goto .end;
-    je .end			;
-    pop r9			;	rsp += 8; r9 = *rsp;
-    mov rcx, 64			;	rcx = 64;
-    jmp .print_1_digit		;	goto .print_1_digit;
+    syscall			;	rax = read(rdi:stdin, rsi:(buf + rcx), rdx:1):(num of read bytes);
+    pop rcx			;	rcx = *rsp; rsp += 8;//recover rcx
+    cmp byte[rsi], char_space	;	if(*rsi == ' ')goto .success;
+    je .success			;
+    cmp byte[rsi], char_tab	;	if(*rsi == '\t')goto .success;
+    je .success			;
+    cmp byte[rsi], char_new_line;	if(*rsi == '\n')goto .success;
+    je .success			;
+    cmp byte[rsi], char_carriage_return;if(*rsi == 0x0d)goto .success;
+    inc rcx			;	rcx:(num of read bytes)++;
+    inc rsi			;	rsi:(buf + rcx)++;
+    jmp .read			;	goto .read;
+    je .success			;
+.success:			;.success:
+    mov byte[rsi], 0		;	*rsi = '\0';
+    mov rax, r8			;	rax = r8:buf;
+    mov rdx, rcx		;	rdx = rcx:(length of word);
+    jmp .end			;	goto .end;
+.error:				;.error:
+    mov rax, 0			;	rax = NULL;
+    jmp .end			;	goto .end;
 .end:
-    pop rbp			;	rsp += 8; rbp = *rsp;
-    ret				;	return;
+    pop rbp			;	rbp = *rsp; rsp += 8;
+    ret				;	return rax:buf, rdx:(length of word);
 				;}
 
 _start:				;int main(void)
 				;{
-    mov rdi, 12345		;	print_uint(12345);
-    call print_uint		;
+    push rbp			;	*(rsp -= 8) = rbp;
+    mov rbp, rsp		;	rbp = rsp;
+    xor rax, rax		;	rax = 0;
+    push rax			;	*(rsp -= 8) = rax:0;
+    mov rdi, rsp		;	rdi = rsp;
+    mov	rsi, 7			;	rsi = 7;
+    call read_word		;	{rax:buf, rdx:sz} = read_word(rdi:rsp:buf, rsi:7:sz);
+    pop rax			;	rax = *rsp; rsp += 8;
+    pop rbp			;	rbp = *rsp; rsp += 8;
     mov rax, syscall_exit	;	exit(0);
     mov rdi, 0			;
     syscall			;
