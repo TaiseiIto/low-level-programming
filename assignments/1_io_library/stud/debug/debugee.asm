99f6f34e244fdfde83f456cf3ddda26f52ba3cf8
TaiseiIto <taisei1212@outlook.jp>
2020-08-10 16:35:00 +0900

    2.7 debug parse_int

--- assignments/1_io_library/stud/debug/debugee.asm
+++ assignments/1_io_library/stud/debug/debugee.asm
@@ -1,6 +1,7 @@
 char_carriage_return equ 0x0d
 char_minus equ 0x2d
 char_new_line equ 0x0a
+char_nine equ 0x39
 char_null equ 0x00
 char_space equ 0x20
 char_tab equ 0x09
@@ -13,86 +14,84 @@ syscall_write equ 1
 
 global _start
 
+section .data
+
+input: db '-1dasda'
+
 section .text
 
-string_length:			;unsigned long string_length(char *rdi:string)
+; rdi points to a string
+; returns rax: number, rdx : length
+parse_uint:			;{unsigned long rax:(parsed num), unsigned long rdx:(parsed string length)} parse_uint(char *rdi:string)
 				;{
-    xor rax, rax		;	(rax ^= rax):(rax = 0);
-.loop_begin:			;	while(1)
-				;	{
-    cmp byte[rdi + rax], 0x00	;		if(!*rdi:string)break;
-    je .loop_end		;
-    inc rax			;		rax++;
-    jmp .loop_begin		;
-.loop_end:			;	}
-    ret				;	return rax;
+    xor rax, rax		;	(rax:(parsed num) ^= rax):(rax = 0);
+    xor rdx, rdx		;	(rdx:(parsed string length) ^= rdx):(rdx = 0);
+    mov r9, 10			;	r9 = 10;//decimal system
+    xor r10, r10		;	(r10:(parsed digit) ^= r10):(r10 = 0);
+.parse_8_digits:		;.parse_8_digits:
+    mov r8, qword[rdi]		;	char r8[8] = *rdi;
+    xor rcx, rcx		;	(rcx:(parse rcx byte of r8) ^= rcx):(rcx = 0);
+.parse_1_digit:			;.parse_1_digits:
+    cmp r8b, char_zero		;	if(r8[0] < '0')goto .end;
+    jb .end
+    cmp r8b, char_nine		;	if('9' < r8[0])goto .end;
+    ja .end
+    push rdx			;	*(rsp -= 8) = rdx;//protect rdx from mul
+    mul r9			;	rdx:rax:(parsed num) = rax * r9:10;
+    test rdx, rdx		;	if(0 < rdx)goto .too_big;
+    jnz .too_big
+    pop rdx			;	rdx = *rsp; rsp += 8;//recover rdx
+    sub r8b, char_zero		;	r8[0] -= '0';//parsed digit
+    mov r10b, r8b		;	r10b = r8[0]:(parsed digit);
+    add rax, r10		;	rax:(parsed num) += r10:(parsed digit);
+    jc .too_big			;	if(carry_flag)goto .too_big;
+    inc rcx			;	rcx:(parse rcx byte of r8)++;
+    inc rdx			;	rdx:(parsed string length)++;
+    cmp rcx, 8			;	if(rcx:(parse rcx byte of r8) == 8)goto .parse_8_digits;
+    je .parse_next_8_digits
+    sar r8, 8			;	r8 >>= 8;
+    jmp .parse_1_digit		;	goto .parse_1_digit;
+.too_big:			;.too_big:
+    xor rax, rax		;	(rax:(parsed num) ^= rax):(rax = 0);
+    xor rdx, rdx		;	(rdx:(parsed string length) ^= rdx):(rdx = 0);
+    ret				;	return {unsigned long rax:(parsed num):0, unsigned long rdx:(parsed string length):0};
+.parse_next_8_digits:		;.parse_next_8_digits:
+    add rdi, 8			;	rdi:(string address) += 8;
+    jmp .parse_8_digits		;	goto .parse_8_digits;
+.end:
+    ret				;	return {unsigned long rax:(parsed num), unsigned long rdx:(parsed string length)};
 				;}
 
-print_string:			;unsigned long:(num of written bytes) print_string(char *rdi:string)//print string to stdout
+; rdi points to a string
+; returns rax: number, rdx : length
+parse_int:			;{long rax:(parsed num), unsigned long rdx:(parsed string length)} parse_int(char *rdi:string)
 				;{
-    push rdi			;	*(rsp -= 8) = rdi:string;
-    call string_length		;	rax = string_length(rdi:string);
-    mov rdx, rax		;	rdx = rax:string_length(string);
-    mov rax, syscall_write	;	rax = syscall_write;
-    mov rdi, stdout		;	rdi = stdout;
-    pop rsi			;	rsi = (*rsp):string; rsp += 8;
-    syscall			;	rax = write(rdi:stdout, rsi:string, rdx:string_length(string)):(num of written bytes);
-    ret				;	return rax:(num of written bytes);
-				;}
-
-print_uint:			;void print_uint(unsigned long rdi:integer)//print unsigned integer by decimal system
-				;{
-    push rbp			;	*(rsp -= 8) = rbp;
-    mov rbp, rsp		;	rbp = rsp;
-    mov rax, rdi		;	rax = rdi:integer;
-    mov r8, 10			;	r8:divisor = 10;
-    xor r9, r9			;	r9:(8 decimal digits) = 0;
-    test rax, rax		;	if(rax:integer == 0)goto .print_zero;
-    jz .print_zero;
-    mov rcx, 1			;	rcx:(num of stored r9:(8 decimal digits) bytes) = 1;
-.division_begin:		;.division_begin:
-    test rax, rax		;	if(rax == 0)goto .division_end;
-    jz .division_end		;
-    xor rdx, rdx		;	(rdx ^= rdx):(rdx = 0);
-    div r8			;	rdx = rax:rdi % r8:10; rax = rax:rdi / r8:10;
-    shl r9, 8			;	r9 <<= 8;
-    add r9b, dl			;	r9 += rdx:remainder;
-    add r9b, char_zero		;	r9 += '0';
-    inc rcx			;	rcx++;
-    cmp rcx, 8			;	if(rcx != 8)goto .division_begin;
-    jne .division_begin		;
-    push r9			;	*(rsp -= 8) = r9:(8 decimal digits);
-    xor rcx, rcx		;	rcx = 0;
-    jmp .division_begin		;	goto .division_begin;
-.division_end:			;.division_end:
-    xor rax, rax		;	rax = 0;
-    test rcx, rcx		;	if(!rcx)goto .print;
-    jz .print			;
-    mov rax, 8			;	rax = 8;
-    sub rax, rcx		;	(rax -= rcx):(rax = 8 - rcx);
-    mov rdx, rax		;	rdx = rax:(8 - rcx);
-    shl rdx, 3			;	(rdx <<= 3):(rdx = 8 * (8 - rcx));
-.shift_last_8_decimal_digits:	;.shift_last_8_decimal_digits
-    sal r9, 1			;	r9 <<= rdx:(8 * (8 - rcx));
-    dec rdx			;
-    jnz .shift_last_8_decimal_digits;
-    push r9			;	*(rsp -= 8) = r9:(rcx decimal digits);
-.print:				;.print
-    mov rdi, rsp		;	rdi = rsp;
-    add rdi, rax		;	(rdi += rax):(rdi = address of decimal string);
-    call print_string		;	print_string(rdi:(address of decimal string));
-    leave			;	rsp = rbp; rbp = *rsp; rsp -= 8;
-    ret				;	return;
-.print_zero:			;.print_zero:
-    mov r9b, char_zero		;	r9b = '0';
-    mov rax, 7			;	rax = 7:(num of blank bytes of r9);
-    mov rdx, 56			;	rdx = 56:(num of blank bits of r9);
-    jmp .shift_last_8_decimal_digits;	goto .shift_last_8_decimal_digits;
+    cmp byte[rdi], char_minus	;	if(*rdi == '-')goto .minus;
+    je .minus
+    call parse_uint		;	{rax, rdx} = parse_uint(rdi:string);
+    mov r8, 0x8000000000000000	;	r8 = -1;//check range
+    test rax, r8		;	if(rax < 0)goto .error;
+    jnz .error
+    jmp .end			;	else goto .end;
+.minus:				;.minus:
+    inc rdi			;	rdi:string++;//skip '-'
+    call parse_uint		;	{rax, rdx} = parse_uint(rdi:(abs part string));
+    mov r8, 0x8000000000000000	;	r8 = -1;//check range
+    cmp rax, r8			;	if(0x8000000000000000 rax)goto .error;
+    ja .error
+    neg rax			;	rax:(parsed num) *= -1;
+    inc rdx			;	rdx:(parsed string length)++;
+.error:				;.error:
+    xor rax, rax		;	(rax:(parsed num) ^= rax):(rax = 0);
+    xor rdx, rdx		;	(rdx:(parsed string length) ^= rdx):(rdx = 0);
+    ret				;	return {long rax:(parsed num):0, unsigned long rdx:(parsed string length):0};
+.end:				;.end:
+    ret 			;	return {long rax:(parsed num), unsigned long rdx:(parsed string length)};
 				;}
 
 _start:
-    xor rdi, rdi
-    call print_uint
+    mov rdi, input
+    call parse_int
     mov rax, syscall_exit
     xor rdi, rdi
     syscall
