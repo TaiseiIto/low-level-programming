80c5a6790d82145cbb6cee610d8cdc8097ed68d6
TaiseiIto <taisei1212@outlook.jp>
2020-08-12 19:39:34 +0900

    q66

--- questions/stud/66/stat.asm
+++ questions/stud/66/stat.asm
@@ -1,9 +1,31 @@
+%define CHAR_NEWLINE 0x0a
+%define CHAR_NULL 0x00
 %define SYSCALL_EXIT 0x000000000000003c
+
 global _start
+
+section .data
+
+no_file_name_message: db 'NO FILE NAME!', CHAR_NEWLINE, CHAR_NULL	;char *no_file_name_message = "NO FILE NAME!\n";
 
 section .text
 
-_start:
-	mov rax, SYSCALL_EXIT
-	xor rdi, rdi
-	syscall
+error:					;void error(char *rdi:message)
+					;{
+	mov rax, SYSCALL_EXIT		;
+	mov rdi, 1			;
+	syscall				;	exit(1);
+					;}
+
+_start:					;int main(int argc, char **argv)
+					;{
+	cmp qword[rsp], 1		;
+	je .no_file_name		;	if(argc == 1)goto .no_file_name;
+	mov rax, SYSCALL_EXIT		;
+	xor rdi, rdi			;
+	syscall				;	exit(0);
+.no_file_name:				;.no_file_name:
+	mov rdi, no_file_name_message	;
+	call error			;	error(rdi:no_file_name_message);
+					;}
+
